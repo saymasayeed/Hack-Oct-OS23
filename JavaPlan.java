b33d96d0b4bd33037006bfcd66d6e73c4ac7cb7f
Rishabh <113057571+mvp-2003@users.noreply.github.com>
2023-10-11 13:04:43 +0530

    Create JavaPlan.java

---
+++ JavaPlan.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class JavaCall{
+  String element;
+  int a, b;
+
+  public showData(){
+    System.out.println(this.a);
+    System.out.println(this.name);
+}
+  
+public class JavaPlan{
+  public static void main(String args[]);
+  JavaCall object1 = new JavaCall(System.in);
+  object1.name = "Undefined";
+  object1.a = 55;
+  object1.b = 57;
+  object1.showData();
+}
